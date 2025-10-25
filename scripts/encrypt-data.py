cbe019fd15e5b27e6e3ca3db9005d758b43146ac
toftamars <toftamars@users.noreply.github.com>
2025-10-25 13:46:42 +0300

    🎉 SEÇENEK B TAMAMLANDI - Veri bölündü ve şifrelendi

--- scripts/encrypt-data.py
+++ scripts/encrypt-data.py
@@ -9,7 +9,7 @@ import json
 import gzip
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 import base64
 
@@ -20,7 +20,7 @@ def generate_key(password, salt=None):
     if salt is None:
         salt = os.urandom(16)
     
-    kdf = PBKDF2(
+    kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
